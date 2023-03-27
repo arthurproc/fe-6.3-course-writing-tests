b30c30f84d51d01c5b08234cdbaaab46f9e0771b
Arthur Procópio <avprocopio@gmail.com>
2023-03-27 19:18:20 -0300

    add code

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,31 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
 import App from './App';
+import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('deve renderizar o componente App', () => {
+  renderWithRouter(<App />);
+
+  const homeTitle = screen.getByRole('heading', {
+    name: 'Você está na página Início',
+  });
+  expect(homeTitle).toBeInTheDocument();
+});
+
+
+it('deve renderizar o componente Sobre', async () => {
+  const { history } = renderWithRouter(<App />);
+
+  const aboutLink = screen.getByRole('link', { name: 'Sobre' });
+  expect(aboutLink).toBeInTheDocument();
+  userEvent.click(aboutLink);
+
+  const { pathname } = history.location;
+  expect(pathname).toBe('/about');
+
+  const aboutTitle = screen.getByRole('heading',
+    { name: 'Você está na página Sobre' });
+  expect(aboutTitle).toBeInTheDocument();
 });
